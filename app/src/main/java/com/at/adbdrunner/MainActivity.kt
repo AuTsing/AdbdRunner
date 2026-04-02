41444e9965d3097bd1fe371b602ce0348f6c4ea1
AuTsing <763025696@qq.com>
2026-04-03 03:33:40 +0800

    Add running process

--- app/src/main/java/com/at/adbdrunner/MainActivity.kt
+++ app/src/main/java/com/at/adbdrunner/MainActivity.kt
@@ -2,9 +2,35 @@ package com.at.adbdrunner
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
+import java.io.DataOutputStream
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        runCatching {
+            val rt = Runtime.getRuntime()
+            val p = rt.exec("su")
+            val cmds = listOf(
+                "setprop service.adb.tcp.port 5555\n",
+                "stop adbd\n",
+                "start adbd\n",
+                "exit\n",
+            )
+            DataOutputStream(p.outputStream).use { dos ->
+                for (cmd in cmds) {
+                    dos.writeBytes(cmd)
+                    dos.flush()
+                }
+            }
+            p.waitFor()
+        }.fold(
+            onSuccess = { it },
+            onFailure = { it.message },
+        ).also {
+            Toast.makeText(this, "Exit with: $it", Toast.LENGTH_LONG).show()
+            finishAndRemoveTask()
+        }
     }
 }
